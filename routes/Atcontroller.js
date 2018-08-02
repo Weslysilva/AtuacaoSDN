681645478552ea0f3f43751b6ac263c5f88a5f59
Unknown <wesly.s.silva@gmail.com>
2018-08-02 17:49:45 -0300

    Funcao que altera o DSCP
    AINDA NAO TESTADO POR FALTA DO OBJETO JSON CORRELATO.

--- routes/Atcontroller.js
+++ routes/Atcontroller.js
@@ -43,7 +43,7 @@ router.get('/getDevice', async function(req, res, next) {
 
 
 //Coleta a tabela de encaminhamento de um dispositivo.
-router.get('/get', async function(req, res, next) {
+router.get('/getPathForward', async function(req, res, next) {
 
   var token = await mid.obterToken(false)
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -58,6 +58,83 @@ router.get('/get', async function(req, res, next) {
       });
 
 });
+
+
+//Coleta os dados de uma plano de encaminhamento de um dispositivo.
+router.get('/getDataPath', async function(req, res, next) {
+
+  var token = await mid.obterToken(false)
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    request.get({
+        headers: {'content-type' : 'application/json', 'X-Auth-Token' : token},
+        url:     'https://10.192.168.121:8443/sdn/v2.0/of/datapaths/' + `${req.dpid}`, //Utilizar ID do Elemento de Rede.
+        body:  null,
+        json : true
+      }, function(error, body, response){
+        if(error) { console.log("Atenção! houve um erro durante a requisição ao controlador\n segue abaixo o log:\n\n"+error) } 
+        res.send(response);
+      });
+
+});
+
+
+//Coleta as metricas 
+router.get('/getMeter', async function(req, res, next) {
+
+  var token = await mid.obterToken(false)
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    request.get({
+        headers: {'content-type' : 'application/json', 'X-Auth-Token' : token},
+        url:     'https://10.192.168.121:8443/sdn/v2.0/of/datapaths/' + `${req.dpid}` + '/features/meter', //o Datapath ID deve ser passado na Requsição.
+        body:  null,
+        json : true
+      }, function(error, body, response){
+        if(error) { console.log("Atenção! houve um erro durante a requisição ao controlador\n segue abaixo o log:\n\n"+error) } 
+        res.send(response);
+      });
+
+});
+
+//Edita as metricas 
+router.get('/getMeter', async function(req, res, next) {
+
+  var token = await mid.obterToken(false)
+
+    var meters = {
+      "version": "1.3.0",
+      "meter_features": {
+        "max_meters": 8,
+        "types": [
+          "drop",
+          "dscp_remark"
+        ],
+        "flags": [
+          "kbps",
+          "pktps"
+        ],
+        "max_bands_per_meter": 3,
+        "max_color_value": 0
+      }
+    }
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    request.get({
+        headers: {'content-type' : 'application/json', 'X-Auth-Token' : token},
+        url:     'https://10.192.168.121:8443/sdn/v2.0/of/datapaths/' + `${req.dpid}` + '/features/meters', //o Datapath ID deve ser passado na Requsição.
+        body:  meters,
+        json : true
+      }, function(error, body, response){
+        if(error) { console.log("Atenção! houve um erro durante a requisição ao controlador\n segue abaixo o log:\n\n"+error) } 
+        res.send(response);
+      });
+
+});
+
+
+
+
+
+
 
 
 module.exports = router;
